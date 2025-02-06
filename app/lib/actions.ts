11e02daa028cd20d255b758233e50b93198f3dd8
frtavonatti <frtavonatti@gmail.com>
2025-02-06 19:55:29 +0000

    Add error handling and not found page for invoice editing; implement error component

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,15 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to create new Invoice'); 
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
